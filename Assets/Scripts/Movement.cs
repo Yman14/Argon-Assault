8cc42af9fda4cc3df7a55430620743be8be53669
Yman <ymanneil07@gmail.com>
2024-07-01 19:53:13 +0800

    player trail position and rotation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,25 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] float thrustSpeed = 200f;
+    [SerializeField] InputAction movement;
+    [SerializeField] float controlSpeed = 40f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 8f;
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -15f;
+
+    float xControl, yControl;
+
+    void OnEnable() {
+        movement.Enable();
+    }
+    void OnDisable() {
+        movement.Disable();
+    }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate (Vector3.forward * thrustSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate (Vector3.down * thrustSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate (Vector3.up * thrustSpeed * Time.deltaTime);
-        }
+        ProcessTranslation();
+        ProcessRotation();
 
+    }
+
+    void ProcessTranslation()
+    {
+        xControl = movement.ReadValue<Vector2>().x;
+        yControl = movement.ReadValue<Vector2>().y;
+
+
+        float xOffset = xControl * Time.deltaTime * controlSpeed;
+        float yOffset = yControl * Time.deltaTime * controlSpeed;
+        float xRawPos = transform.localPosition.x + xOffset;
+        float yRawPos = transform.localPosition.y + yOffset;
+        float xclampedPos = Mathf.Clamp(xRawPos, -xRange, xRange);
+        float yclampedPos = Mathf.Clamp(yRawPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(xclampedPos, yclampedPos, transform.localPosition.z);
+    }
+
+    void ProcessRotation()
+    {
+
+        float pitch = transform.localPosition.y * positionPitchFactor + yControl * controlPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xControl * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
